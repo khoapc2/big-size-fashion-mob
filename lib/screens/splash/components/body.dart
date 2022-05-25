3a8f5de53e01261b46a8c8e07293c89e1e81ddf9
hieuttblackpink <trantronghieu369963@gmail.com>
2022-05-25 20:54:44 +0700

    v0.1.1

---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,135 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
+import 'package:shop_app/size_config.dart';
+
+// This is the best practice
+import '../components/splash_content.dart';
+import '../../../components/default_button.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to BIG-SIZE Shop, Let’s shop!",
+      "image": "assets/images/splash_1.png"
+    },
+    {
+      "text": "We help overweight people go shopping \naround Viet Nam",
+      "image": "assets/images/splash_2.png"
+    },
+    {
+      "text": "We show the easy way to shop. \nJust stay at home with us",
+      "image": "assets/images/splash_3.png"
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContent(
+                  image: splashData[index]["image"],
+                  text: splashData[index]['text'],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        splashData.length,
+                        (index) => buildDot(index: index),
+                      ),
+                    ),
+                    Spacer(flex: 3),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(30)),
+                                      side: BorderSide(color: kPrimaryColor)))),
+                      child: Container(
+                        width: 250,
+                        height: 60,
+                        alignment: Alignment.center,
+                        //color: kPrimaryColor,
+                        decoration: new BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100.00),
+                          ),
+                          color: kPrimaryColor,
+                        ),
+                        child: Text(
+                          "Shopping now",
+                          style: TextStyle(
+                            fontFamily: "QuickSand",
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                      onPressed: () => {
+                        Navigator.pushNamed(context, SignInScreen.routeName)
+                      },
+                    ),
+                    /*
+                    DefaultButton(
+                      text: "Shopping now",
+                      press: () {
+                        Navigator.pushNamed(context, SignInScreen.routeName);
+                      },
+                    ),
+                    */
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int? index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: EdgeInsets.only(right: 5),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? kPrimaryColor : Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash/components/splash_content.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key? key,
+    this.text,
+    this.image,
+  }) : super(key: key);
+  final String? text, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Spacer(),
+        Text(
+          "BIG SIZE",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(50),
+            //fontFamily: "QuickSandBold",
+            color: kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          text!,
+          style: TextStyle(
+            color: kSecondaryColor,
+            fontSize: 20,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Spacer(flex: 2),
+        Image.asset(
+          image!,
+          height: getProportionateScreenHeight(265),
+          width: getProportionateScreenWidth(235),
+        ),
+      ],
+    );
+  }
+}
