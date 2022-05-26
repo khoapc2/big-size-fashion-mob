a7c68c3788744768817aba2cdf1389439df1400f
longdaungo <zedvidieu@gmail.com>
2022-05-26 08:23:49 +0700

    - basic template

---
+++ lib/screens/cart/components/check_out_card.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shop_app/components/default_button.dart';
+import 'package:shop_app/screens/payment/payment.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class CheckoutCard extends StatelessWidget {
+  const CheckoutCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: getProportionateScreenWidth(15),
+        horizontal: getProportionateScreenWidth(30),
+      ),
+      // height: 174,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -15),
+            blurRadius: 20,
+            color: Color(0xFFDADADA).withOpacity(0.15),
+          )
+        ],
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "Total:\n",
+                    children: [
+                      TextSpan(
+                        text: "\$337.15",
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: getProportionateScreenWidth(190),
+                  child: DefaultButton(
+                    text: "Check Out",
+                    press: () {
+                      Navigator.push(
+                        context,
+                      MaterialPageRoute(builder: (context) => const PaymentPage()),
+                    );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
